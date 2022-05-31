7268282d093b665cd456796b03c2112bf454d5e8
gams <gabriel.antonioms@gmail.com>
2022-05-30 23:18:59 -0300

    v.2.0.0 - add P.O and PyQT5

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import time
 from layouts.layout import *
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
@@ -5,21 +6,53 @@ class Aplicacao(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         super().setupUi(self)
+        self.maOutput.setReadOnly(1)
+        self.vcOutput.setReadOnly(1)
+        self.vac0Output.setReadOnly(1)
+        self.vac1Output.setReadOnly(1)
+        self.vbc0Output.setReadOnly(1)
+        self.vbc1Output.setReadOnly(1)
+        self.mac0Output.setReadOnly(1)
+        self.mac1Output.setReadOnly(1)
+        self.mbc0Output.setReadOnly(1)
+        self.mbc1Output.setReadOnly(1)
         self.btnCalcular.clicked.connect(self.calcular)
 
     def calcular(self):
+
+        self.statusBar().setStyleSheet("background-color :")
+        self.statusBar().showMessage("")
         va, vb, x1, x2 = self.recebe_valores()
-        print(f'Va = {va},Vb = {vb}, x1= {x1}')
+        vc, ma = self.reacoes_de_apoio(va, vb, x1, x2)
+        vac0, vac1, vbc0, vbc1 = self.forca_cortante(va, vb, x1, x2)
+        mac0, mac1, mbc0, mbc1 = self.momento_fletor(va, vb, x1, x2)
+        self.maOutput.setText(str(ma))
+        self.vcOutput.setText(str(vc))
+        self.vac0Output.setText(str(vac0))
+        self.vac1Output.setText(str(vac1))
+        self.vbc0Output.setText(str(vbc0))
+        self.vbc1Output.setText(str(vbc1))
+        self.mac0Output.setText(str(mac0))
+        self.mac1Output.setText(str(mac1))
+        self.mbc0Output.setText(str(mbc0))
+        self.mbc1Output.setText(str(mbc1))
+
 
     def recebe_valores(self):
-        va = float(self.vaInput.text())
-        vb = float(self.vbInput.text())
-        x1 = float(self.x1Input.text())
-        x2 = float(self.x2Input.text())
+        try:
+            va = float(self.vaInput.text())
+            vb = float(self.vbInput.text())
+            x1 = float(self.x1Input.text())
+            x2 = float(self.x2Input.text())
 
-        return va, vb, x1, x2
+            return va, vb, x1, x2
+        except:
+            self.statusBar().setStyleSheet("background-color : red")
+            self.statusBar().showMessage("Digite apenas números inteiros ou números no formato americano.")
+            return False, False, False, False
 
-    def forca_cortante(va, vb, x1, x2):
+
+    def forca_cortante(self, va, vb, x1, x2):
         vac0 = va - 0  # para x = 0
         vac1 = va - x1  # para x = x1
         vbc0 = vb - 0  # para x = 0
@@ -27,13 +60,13 @@ class Aplicacao(QMainWindow, Ui_MainWindow):
 
         return vac0, vac1, vbc0, vbc1
 
-    def reacoes_de_apoio(va, vb, vc, x1, x2):
+    def reacoes_de_apoio(self, va, vb, x1, x2):
         vc = va + vb
         ma = (vc * x1) - (vb * (x2 + x1))
 
         return vc, ma
 
-    def momento_fletor(va, vb, x1, x2):
+    def momento_fletor(self, va, vb, x1, x2):
         mac0 = va - 0
         mac1 = va - ((x1 ** 2) / 2)
         mbc0 = vb - 0
--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 import sys
 from application import *
 
-
-
 if __name__ == '__main__':
-
-    # va = input('insira VA:')
-    # vb = input('insira VB:')
-    # vc = input('insira VC:')
-    # x1 = input('insira x1:')
-    # x2 = input('insira x2:')
-
-    va = 5; vb = 8; x1 = 10; x2 = 8
 
     qt= QApplication(sys.argv)
     app = Aplicacao()
     app.show()
     qt.exec()
-    vc, ma = reacoes_de_apoio(va, vb, x1, x2)
-    vac0, vac1, vbc0, vbc1 = forca_cortante(va, vb, x1, x2)
-    mac0, mac1, mbc0, mbc1 = momento_fletor(va, vb, x1, x2)
-
-    # print(f'Reações de apoio:\nVc = {vc} e Ma = {ma}')
-    # print(f'\nForça cortante:\nVac (x = 0) = {vac0}')
-    # print(f'Vac (x = x1) = {vac1}')
-    # print(f'Vbc (x = 0) = {vbc0}')
-    # print(f'Vbc (x = x2) = {vbc1}')
-    # print(f'\nMomento fletor:\nMac (x = 0) = {mac0}')
-    # print(f'Mac (x = x1) = {mac1}')
-    # print(f'Mbc (x = 0) = {mbc0}')
-    # print(f'Mbc (x = x2) = {mbc1}')
 
 
 
